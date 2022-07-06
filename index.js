1929426412c8e86d8e4c503af3bfe605675686cb
Abdul Salam <60816007+AbdulSalam416@users.noreply.github.com>
2022-07-06 15:17:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -34,28 +34,6 @@ function triggerFunc(e) {
 
 
 
-// document.addEventListener("click", function(event) {
-
-//         let dropdownItems = document.querySelectorAll(".dropdown-list");
-
-
-
-//         if (!event.target.classList.contains("features-target")) {
-//             var i;
-//             for (i = 0; i < dropdownItems.length; i++) {
-//                 var openDropdown = dropdownItems[i][i];
-//                 console.log(openDropdown.classList)
-//                 if (openDropdown.classList.contains('features')) {
-//                     openDropdown.classList.remove('features');
-//                 }
-//             }
-//         }
-
-//     }
-
-
-
-// )
 
 dropdowns.forEach(dropdown => dropdown.addEventListener("click", triggerFunc));
 // dropdowns.forEach(dropdown => dropdown.addEventListener("mouseover", triggerFunc));
